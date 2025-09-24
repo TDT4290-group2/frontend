9ca99b4dee32698431707c8e15dbd72cf3ae0527
Martin Renslo Høva <126882450+martinhova01@users.noreply.github.com>
2025-09-24 14:28:40 +0200

    feat: add navigation bar component (#3) (#14)
    * feat: add navigation bar component (#3)
    
    * refactor: change a to NavLink, remove Sidebar (#3)
    
    * refactor: made navbar more composable (#3)
    
    * feat: :sparkles: add navbar
    
    * feat: :sparkles: add nav drawer for mobile view
    
    * style: :lipstick: fix navbar item rounding
    
    * style: :art: sort imports
    
    * chore: :heavy_minus_sign: remove unused deps
    
    * chore: :heavy_minus_sign: remove unused deps
    
    ---------
    
    Co-authored-by: phibkro <71797726+phibkro@users.noreply.github.com>

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,16 @@
-import { index, layout, type RouteConfig } from "@react-router/dev/routes";
+import {
+	index,
+	layout,
+	type RouteConfig,
+	route,
+} from "@react-router/dev/routes";
 
 // biome-ignore lint: default export in config files is fine
 export default [
-	layout("routes/sidebar.tsx", [index("routes/home.tsx")]),
+	layout("routes/layout.tsx", [
+		index("routes/home.tsx"),
+		route("/dust", "routes/dust.tsx"),
+		route("/vibration", "routes/vibration.tsx"),
+		route("/noise", "routes/noise.tsx"),
+	]),
 ] satisfies RouteConfig;
---
+++ app/routes/vibration.tsx
@@ -0,0 +1,4 @@
+// biome-ignore lint: page components can be default exports
+export default function Vibration() {
+	return <p>{"This is the vibration page"}</p>;
+}
