401c2f98c0c2159625624dab34b6c736ea24154f
HermanOstenby2001 <hermanostenby@gmail.com>
2025-09-25 16:13:53 +0200

    feat: Initial graphs and calendars for overview page(#12)

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,36 @@
 import { cn, parseAsView } from "@/lib/utils";
 import { useQueryState } from "nuqs";
 import { Calendar } from "../components/ui/calendar";
+import { Card } from "../components/ui/card";
+import { ChartLine } from "lucide-react";
+import { WeeklyOverview } from "../components/weekly";
+import { ChartLineDefault, ThresholdLine } from "../components/line-chart";
+// import noiseChartData from "../dummy/noise_chart_data.json";
+// import vibrationChartData from "../dummy/vibration_chart_data.json";
+// import dustChartData from "../dummy/dust_chart_data.json";
+
+// const noiseData = noiseChartData;
+// const vibrationData = vibrationChartData;
+// const dustData = dustChartData;
+
+const noiseChartData = [
+	{ x: "08:00", y: 75 },
+	{ x: "10:00", y: 88 },
+	{ x: "12:00", y: 92 },
+	{ x: "14:00", y: 85 }
+];
+const vibrationChartData = [
+	{ x: "08:00", y: 75 },
+	{ x: "10:00", y: 88 },
+	{ x: "12:00", y: 92 },
+	{ x: "14:00", y: 85 }
+];
+const dustChartData = [
+	{ x: "08:00", y: 75 },
+	{ x: "10:00", y: 88 },
+	{ x: "12:00", y: 92 },
+	{ x: "14:00", y: 85 }
+];
 
 export function meta() {
 	return [
@@ -13,9 +43,9 @@ export function meta() {
 export default function Home() {
 	const [view] = useQueryState("view", parseAsView.withDefault("day"));
 
-	const greenDays = [new Date(2025, 8, 1), new Date(2025, 8, 5)];
-	const yellowDays = [new Date(2025, 8, 2), new Date(2025, 8, 6)];
-	const redDays = [
+	const greenNoiseDays = [new Date(2025, 8, 1), new Date(2025, 8, 5)];
+	const yellowNoiseDays = [new Date(2025, 8, 2), new Date(2025, 8, 6)];
+	const redNoiseDays = [
 		new Date(2025, 8, 3),
 		new Date(2025, 8, 7),
 		new Date(2025, 8, 8),
@@ -25,29 +55,58 @@ export default function Home() {
 		case "day":
 			return (
 				<div>
-					<span>{"Overview page"}</span>
-					<span>{"Day view"}</span>
+					<div className="p-2">
+						<ChartLineDefault
+							chartData={noiseChartData}
+							chartTitle="Noise Exposure"
+							unit="db (TWA)"
+							>
+							<ThresholdLine y={120} dangerLevel="DANGER" />
+						</ChartLineDefault>
+					</div>
+					<div className="p-2">
+						<ChartLineDefault
+							chartData={vibrationChartData}
+							chartTitle="Vibration Exposure"
+							unit="db (TWA)"
+						>
+							<ThresholdLine y={120} dangerLevel="DANGER" />
+						</ChartLineDefault>
+					</div>
+					<div className="p-2">
+						<ChartLineDefault
+							chartData={dustChartData}
+							chartTitle="Dust Exposure"
+							unit="db (TWA)"
+						>
+							<ThresholdLine y={120} dangerLevel="DANGER" />
+						</ChartLineDefault>
+					</div>
 				</div>
 			);
 		case "week":
 			return (
 				<div>
-					<span>{"Overview page"}</span>
-					<span>{"Week view"}</span>
+					<WeeklyOverview />;
+					<WeeklyOverview />;
+					<WeeklyOverview />;
 				</div>
 			);
 		case "month":
 			return (
-				<main className="grid h-full w-full place-content-center">
+				<div>
+					<div className="p-2">
+					<Card className="sm: w-full md:w-4/5 lg:w-3/4">
 					<Calendar
 						fixedWeeks
 						showWeekNumber
 						disabled
 						mode="single"
+						weekStartsOn={1}
 						modifiers={{
-							safe: greenDays,
-							warning: yellowDays,
-							danger: redDays,
+							safe: greenNoiseDays,
+							warning: yellowNoiseDays,
+							danger: redNoiseDays,
 						}}
 						modifiersClassNames={{
 							safe: cn("bg-green-500 dark:bg-green-700"),
@@ -55,11 +114,63 @@ export default function Home() {
 							danger: cn("bg-red-500 dark:bg-red-700"),
 							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
 						}}
-						className="rounded-md border font-bold text-foreground shadow-sm [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(11)] md:[--cell-size:--spacing(20)]"
+						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
 						captionLayout="dropdown"
 						buttonVariant="ghost"
 					/>
-				</main>
+					</Card>
+					</div>
+					<div className="p-2">
+					<Card className="sm: w-full md:w-4/5 lg:w-3/4">
+					<Calendar
+						fixedWeeks
+						showWeekNumber
+						disabled
+						mode="single"
+						weekStartsOn={1}
+						modifiers={{
+							safe: greenNoiseDays,
+							warning: yellowNoiseDays,
+							danger: redNoiseDays,
+						}}
+						modifiersClassNames={{
+							safe: cn("bg-green-500 dark:bg-green-700"),
+							warning: cn("bg-orange-500 dark:bg-orange-700"),
+							danger: cn("bg-red-500 dark:bg-red-700"),
+							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+						}}
+						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+						captionLayout="dropdown"
+						buttonVariant="ghost"
+					/>
+					</Card>
+					</div>
+					<div className="p-2">
+					<Card className="sm: w-full md:w-4/5 lg:w-3/4">
+					<Calendar
+						fixedWeeks
+						showWeekNumber
+						disabled
+						mode="single"
+						weekStartsOn={1}
+						modifiers={{
+							safe: greenNoiseDays,
+							warning: yellowNoiseDays,
+							danger: redNoiseDays,
+						}}
+						modifiersClassNames={{
+							safe: cn("bg-green-500 dark:bg-green-700"),
+							warning: cn("bg-orange-500 dark:bg-orange-700"),
+							danger: cn("bg-red-500 dark:bg-red-700"),
+							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+						}}
+						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+						captionLayout="dropdown"
+						buttonVariant="ghost"
+					/>
+					</Card>
+					</div>
+				</div>
 			);
 		default: {
 			const exhaustiveCheck: never = view;
