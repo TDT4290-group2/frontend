56420368dba4c9a8b1bb09e1a6e3e10f8ebfefc0
Martin Renslo Høva <126882450+martinhova01@users.noreply.github.com>
2025-09-25 23:44:10 +0200

    feat: add components to vibration page (#28) (#31)
    * feat: add components in noise page (#29)
    
    * feat: add warning line in graph (#29)
    
    * feat: add components to vibration page (#28)
    
    ---------
    
    Co-authored-by: phibkro <71797726+phibkro@users.noreply.github.com>

--- app/routes/vibration.tsx
+++ app/routes/vibration.tsx
@@ -1,5 +1,31 @@
-import { parseAsView } from "@/lib/utils";
+import { cn, parseAsView } from "@/lib/utils";
 import { useQueryState } from "nuqs";
+import { ChartLineDefault, ThresholdLine } from "../components/line-chart";
+import { Calendar } from "../components/ui/calendar";
+import { Card } from "../components/ui/card";
+import { WeeklyOverview } from "../components/weekly";
+import vibrationChartData from "../dummy/vibration_chart_data.json";
+
+const data = vibrationChartData;
+
+const greenDays = [
+	new Date(2025, 8, 1),
+	new Date(2025, 8, 5),
+	new Date(2025, 8, 4),
+	new Date(2025, 8, 9),
+	new Date(2025, 8, 10),
+	new Date(2025, 8, 12),
+	new Date(2025, 8, 15),
+	new Date(2025, 8, 16),
+	new Date(2025, 8, 18),
+	new Date(2025, 8, 19),
+];
+const yellowDays = [new Date(2025, 8, 2), new Date(2025, 8, 11)];
+const redDays = [
+	new Date(2025, 8, 3),
+	new Date(2025, 8, 17),
+	new Date(2025, 8, 8),
+];
 
 // biome-ignore lint: page components can be default exports
 export default function Vibration() {
@@ -8,24 +34,42 @@ export default function Vibration() {
 	switch (view) {
 		case "day":
 			return (
-				<div>
-					<span>{"Vibration page"}</span>
-					<span>{"Day view"}</span>
-				</div>
+				<ChartLineDefault
+					chartData={data}
+					chartTitle="Vibration Exposure"
+					unit="SEP"
+				>
+					<ThresholdLine y={100} dangerLevel="DANGER" />
+					<ThresholdLine y={80} dangerLevel="WARNING" />
+				</ChartLineDefault>
 			);
 		case "week":
-			return (
-				<div>
-					<span>{"Vibration page"}</span>
-					<span>{"Week view"}</span>
-				</div>
-			);
+			return <WeeklyOverview />;
 		case "month":
 			return (
-				<div>
-					<span>{"Vibration page"}</span>
-					<span>{"Month view"}</span>
-				</div>
+				<Card className="sm: w-full md:w-4/5 lg:w-3/4">
+					<Calendar
+						fixedWeeks
+						showWeekNumber
+						disabled
+						mode="single"
+						weekStartsOn={1}
+						modifiers={{
+							safe: greenDays,
+							warning: yellowDays,
+							danger: redDays,
+						}}
+						modifiersClassNames={{
+							safe: cn("bg-green-500 dark:bg-green-700"),
+							warning: cn("bg-orange-500 dark:bg-orange-700"),
+							danger: cn("bg-red-500 dark:bg-red-700"),
+							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+						}}
+						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+						captionLayout="dropdown"
+						buttonVariant="ghost"
+					/>
+				</Card>
 			);
 		default: {
 			const exhaustiveCheck: never = view;
