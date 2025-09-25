e623590b74b8bb62fca85463b369d4a975aa5389
Philip B. Krogh <71797726+phibkro@users.noreply.github.com>
2025-09-25 11:00:18 +0200

    feat: add customizable calendar component (#22)
    * feat: add calendar component (#18)
    
    Functionality is fine but needs styling improvements.
    
    * fix: :lipstick: fix calendar styling using daypicker props
    
    https://daypicker.dev/guides/custom-modifiers
    
    * style: :lipstick: update styling
    
    Updated safe, warning and danger colors. Updated box margins.
    
    * chore: make calendar more responsive (#18)
    
    ---------
    
    Co-authored-by: HermanOstenby2001 <hermanostenby@gmail.com>
    Co-authored-by: Martin Høva <mrhova46@gmail.com>

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,5 @@
-import { ChartLineDefault, ThresholdLine } from "@/components/line-chart";
+import { Calendar } from "@/components/ui/calendar";
+import { cn } from "@/lib/utils";
 
 export function meta() {
 	return [
@@ -9,7 +10,7 @@ export function meta() {
 
 // biome-ignore lint: page components can be default exports
 export default function Home() {
-	const chartData = [
+	const _chartData = [
 		{ x: "08.10", y: 18 },
 		{ x: "09.05", y: 30 },
 		{ x: "10.01", y: 23 },
@@ -17,15 +18,37 @@ export default function Home() {
 		{ x: "15.32", y: 20 },
 		{ x: "16.01", y: 21 },
 	];
+
+	const greenDays = [new Date(2025, 8, 1), new Date(2025, 8, 5)];
+	const yellowDays = [new Date(2025, 8, 2), new Date(2025, 8, 6)];
+	const redDays = [
+		new Date(2025, 8, 3),
+		new Date(2025, 8, 7),
+		new Date(2025, 8, 8),
+	];
+
 	return (
-		<div>
-			<ChartLineDefault
-				chartData={chartData}
-				chartTitle="Dust Exposure Graph"
-				unit="decibel"
-			>
-				<ThresholdLine y={25} dangerLevel="DANGER" />
-			</ChartLineDefault>
+		<div className="grid h-full w-full place-content-center">
+			<Calendar
+				fixedWeeks
+				showWeekNumber
+				disabled
+				mode="single"
+				modifiers={{
+					safe: greenDays,
+					warning: yellowDays,
+					danger: redDays,
+				}}
+				modifiersClassNames={{
+					safe: cn("bg-green-500 dark:bg-green-700"),
+					warning: cn("bg-orange-500 dark:bg-orange-700"),
+					danger: cn("bg-red-500 dark:bg-red-700"),
+					disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+				}}
+				className="rounded-md border font-bold text-foreground shadow-sm [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(11)] md:[--cell-size:--spacing(20)]"
+				captionLayout="dropdown"
+				buttonVariant="ghost"
+			/>
 		</div>
 	);
 }
