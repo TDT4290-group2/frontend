3347de3fc5c5d0159df516788aca7fc441ed980d
phibkro <71797726+phibkro@users.noreply.github.com>
2025-09-26 12:58:24 +0200

    refactor: :wheelchair: use semantic html

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
 import { cn, parseAsView } from "@/lib/utils";
 import { useQueryState } from "nuqs";
+import { ChartLineDefault, ThresholdLine } from "../components/line-chart";
 import { Calendar } from "../components/ui/calendar";
 import { Card } from "../components/ui/card";
-import { ChartLine } from "lucide-react";
 import { WeeklyOverview } from "../components/weekly";
-import { ChartLineDefault, ThresholdLine } from "../components/line-chart";
+
 // import noiseChartData from "../dummy/noise_chart_data.json";
 // import vibrationChartData from "../dummy/vibration_chart_data.json";
 // import dustChartData from "../dummy/dust_chart_data.json";
@@ -18,19 +18,19 @@ const noiseChartData = [
 	{ x: "08:00", y: 75 },
 	{ x: "10:00", y: 88 },
 	{ x: "12:00", y: 92 },
-	{ x: "14:00", y: 85 }
+	{ x: "14:00", y: 85 },
 ];
 const vibrationChartData = [
 	{ x: "08:00", y: 75 },
 	{ x: "10:00", y: 88 },
 	{ x: "12:00", y: 92 },
-	{ x: "14:00", y: 85 }
+	{ x: "14:00", y: 85 },
 ];
 const dustChartData = [
 	{ x: "08:00", y: 75 },
 	{ x: "10:00", y: 88 },
 	{ x: "12:00", y: 92 },
-	{ x: "14:00", y: 85 }
+	{ x: "14:00", y: 85 },
 ];
 
 export function meta() {
@@ -55,19 +55,19 @@ export default function Home() {
 	switch (view) {
 		case "day":
 			return (
-				<div>
-					<div className="p-2">
+				<main>
+					<section className="p-2">
 						<h1 className="text-3xl">{"Daily dust exposure"}</h1>
 						<ChartLineDefault
 							chartData={noiseChartData}
 							chartTitle="Noise Exposure"
 							unit="db (TWA)"
-							>
+						>
 							<ThresholdLine y={120} dangerLevel="DANGER" />
 							{/* <ThresholdLine y={85} dangerLevel="WARNING" /> */}
 						</ChartLineDefault>
-					</div>
-					<div className="p-2">
+					</section>
+					<section className="p-2">
 						<h1 className="text-3xl">{"Daily vibration exposure"}</h1>
 						<ChartLineDefault
 							chartData={vibrationChartData}
@@ -77,8 +77,8 @@ export default function Home() {
 							<ThresholdLine y={120} dangerLevel="DANGER" />
 							{/* <ThresholdLine y={85} dangerLevel="WARNING" /> */}
 						</ChartLineDefault>
-					</div>
-					<div className="p-2">
+					</section>
+					<section className="p-2">
 						<h1 className="text-3xl">{"Daily noise exposure"}</h1>
 						<ChartLineDefault
 							chartData={dustChartData}
@@ -88,108 +88,108 @@ export default function Home() {
 							<ThresholdLine y={120} dangerLevel="DANGER" />
 							{/* <ThresholdLine y={85} dangerLevel="WARNING" /> */}
 						</ChartLineDefault>
-					</div>
-				</div>
+					</section>
+				</main>
 			);
 		case "week":
 			return (
-				<div>
-					<div className="p-2">
-						<h1 className="text-3xl">{"Weekly dust exposure"}</h1>
+				<main>
+					<section className="p-2">
+						<h2 className="text-3xl">{"Weekly dust exposure"}</h2>
 						<WeeklyOverview />
-					</div>
-					<div className="p-2">
-						<h1 className="text-3xl">{"Weekly vibration exposure"}</h1>
+					</section>
+					<section className="p-2">
+						<h2 className="text-3xl">{"Weekly vibration exposure"}</h2>
 						<WeeklyOverview />
-					</div>
-					<div className="p-2">
-						<h1 className="text-3xl">{"Weekly noise exposure"}</h1>
+					</section>
+					<section className="p-2">
+						<h2 className="text-3xl">{"Weekly noise exposure"}</h2>
 						<WeeklyOverview />
-					</div>
-				</div>
+					</section>
+				</main>
 			);
 		case "month":
 			return (
-				<div>
-					<div className="p-2">
-					<h1 className="text-3xl">{"Monthly dust exposure"}</h1>
-					<Card className="sm: w-full md:w-4/5 lg:w-3/4">
-					<Calendar
-						fixedWeeks
-						showWeekNumber
-						disabled
-						mode="single"
-						weekStartsOn={1}
-						modifiers={{
-							safe: greenNoiseDays,
-							warning: yellowNoiseDays,
-							danger: redNoiseDays,
-						}}
-						modifiersClassNames={{
-							safe: cn("bg-green-500 dark:bg-green-700"),
-							warning: cn("bg-orange-500 dark:bg-orange-700"),
-							danger: cn("bg-red-500 dark:bg-red-700"),
-							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
-						}}
-						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
-						captionLayout="dropdown"
-						buttonVariant="ghost"
-					/>
-					</Card>
-					</div>
-					<div className="p-2">
-					<h1 className="text-3xl">{"Monthly vibration exposure"}</h1>
-					<Card className="sm: w-full md:w-4/5 lg:w-3/4">
-					<Calendar
-						fixedWeeks
-						showWeekNumber
-						disabled
-						mode="single"
-						weekStartsOn={1}
-						modifiers={{
-							safe: greenNoiseDays,
-							warning: yellowNoiseDays,
-							danger: redNoiseDays,
-						}}
-						modifiersClassNames={{
-							safe: cn("bg-green-500 dark:bg-green-700"),
-							warning: cn("bg-orange-500 dark:bg-orange-700"),
-							danger: cn("bg-red-500 dark:bg-red-700"),
-							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
-						}}
-						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
-						captionLayout="dropdown"
-						buttonVariant="ghost"
-					/>
-					</Card>
-					</div>
-					<div className="p-2">
-					<h1 className="text-3xl">{"Monthly noise exposure"}</h1>
-					<Card className="sm: w-full md:w-4/5 lg:w-3/4">
-					<Calendar
-						fixedWeeks
-						showWeekNumber
-						disabled
-						mode="single"
-						weekStartsOn={1}
-						modifiers={{
-							safe: greenNoiseDays,
-							warning: yellowNoiseDays,
-							danger: redNoiseDays,
-						}}
-						modifiersClassNames={{
-							safe: cn("bg-green-500 dark:bg-green-700"),
-							warning: cn("bg-orange-500 dark:bg-orange-700"),
-							danger: cn("bg-red-500 dark:bg-red-700"),
-							disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
-						}}
-						className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
-						captionLayout="dropdown"
-						buttonVariant="ghost"
-					/>
-					</Card>
-					</div>
-				</div>
+				<main>
+					<section className="p-2">
+						<h2 className="text-3xl">{"Monthly dust exposure"}</h2>
+						<Card className="sm: w-full md:w-4/5 lg:w-3/4">
+							<Calendar
+								fixedWeeks
+								showWeekNumber
+								disabled
+								mode="single"
+								weekStartsOn={1}
+								modifiers={{
+									safe: greenNoiseDays,
+									warning: yellowNoiseDays,
+									danger: redNoiseDays,
+								}}
+								modifiersClassNames={{
+									safe: cn("bg-green-500 dark:bg-green-700"),
+									warning: cn("bg-orange-500 dark:bg-orange-700"),
+									danger: cn("bg-red-500 dark:bg-red-700"),
+									disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+								}}
+								className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+								captionLayout="dropdown"
+								buttonVariant="ghost"
+							/>
+						</Card>
+					</section>
+					<section className="p-2">
+						<h2 className="text-3xl">{"Monthly vibration exposure"}</h2>
+						<Card className="sm: w-full md:w-4/5 lg:w-3/4">
+							<Calendar
+								fixedWeeks
+								showWeekNumber
+								disabled
+								mode="single"
+								weekStartsOn={1}
+								modifiers={{
+									safe: greenNoiseDays,
+									warning: yellowNoiseDays,
+									danger: redNoiseDays,
+								}}
+								modifiersClassNames={{
+									safe: cn("bg-green-500 dark:bg-green-700"),
+									warning: cn("bg-orange-500 dark:bg-orange-700"),
+									danger: cn("bg-red-500 dark:bg-red-700"),
+									disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+								}}
+								className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+								captionLayout="dropdown"
+								buttonVariant="ghost"
+							/>
+						</Card>
+					</section>
+					<section className="p-2">
+						<h2 className="text-3xl">{"Monthly noise exposure"}</h2>
+						<Card className="sm: w-full md:w-4/5 lg:w-3/4">
+							<Calendar
+								fixedWeeks
+								showWeekNumber
+								disabled
+								mode="single"
+								weekStartsOn={1}
+								modifiers={{
+									safe: greenNoiseDays,
+									warning: yellowNoiseDays,
+									danger: redNoiseDays,
+								}}
+								modifiersClassNames={{
+									safe: cn("bg-green-500 dark:bg-green-700"),
+									warning: cn("bg-orange-500 dark:bg-orange-700"),
+									danger: cn("bg-red-500 dark:bg-red-700"),
+									disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+								}}
+								className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+								captionLayout="dropdown"
+								buttonVariant="ghost"
+							/>
+						</Card>
+					</section>
+				</main>
 			);
 		default: {
 			const exhaustiveCheck: never = view;
