c04af8e3fcd1082c2dd2e45ba856cf2015991107
haakoaal <haakoaal@stud.ntnu.no>
2025-10-08 14:21:22 +0200

    adjusted design of summary

--- app/components/ui/summary.tsx
+++ app/components/ui/summary.tsx
@@ -5,44 +5,44 @@ export function Summary({exposureType, safeCount, warningCount, dangerCount, saf
     : {exposureType?: Sensor, safeCount: number, warningCount: number, dangerCount: number, safeLabel: string, warningLabel: string, dangerLabel: string}) {
 
         // TODO - Apply universal colors - hardcoded as of now
-        const safeColor = "text-green-700"; // i.e. Replace with dangerLevels[DangerTypes.low].color or something
-        const warningColor = "text-orange-700";
-        const dangerColor = "text-red-700";
+        const safeColor = "text-safe"; // i.e. Replace with dangerLevels[DangerTypes.low].color or something
+        const warningColor = "text-warning";
+        const dangerColor = "text-destructive";
 
     return (
-        <Card className="flex flex-col w-fit md:max-w-1/3 max-w-fit p-5 gap-0 shadow ">
-            <div className="pl-2 pb-2 border-b-2 border-b-slate-300">
-                <h2 className="text-2xl">Exposure Summary</h2>
+        <Card className="flex flex-col w-full p-5 gap-0 shadow ">
+            <div className="md:pl-2 md:pb-2 border-b-2 border-b-slate-300">
+                <h2 className="text-xl text-center md:text-left md:text-2xl">Exposure Summary</h2>
             </div>
             <div className="exposure-subheader">
-                <h4 className="text-sm text-right text-slate-400">
+                <h4 className="text-sm text-center md:text-right text-slate-400">
                     <span> {exposureType ? exposureType : "Every sensor"} </span>
                 </h4>
             </div>
             {/* Safe */}
-            <div className="flex p-2 items-baseline" title={dangerLevels[DangerTypes.low].label}>
+            <div className="flex p-2 justify-center items-baseline md:justify-start" title={dangerLevels[DangerTypes.low].label}>
                 <span className={cn("text-2xl font-bold brightness-110 w-8 text-center", safeColor)}>
                     {safeCount}
                 </span>
-                <span className={cn("text-sm ml-2", safeColor)}>
+                <span className={cn("hidden md:block text-sm ml-2", safeColor)}>
                     {safeLabel}
                 </span>
             </div>
             {/* Warning */}
-            <div className="flex p-2 items-baseline" title={dangerLevels[DangerTypes.medium].label}>
+            <div className="flex p-2 justify-center items-baseline md:justify-start" title={dangerLevels[DangerTypes.medium].label}>
                 <span className={cn("text-2xl font-bold brightness-110 w-8 text-center", warningColor)}>
                     {warningCount}
                 </span>
-                <span className={cn("text-sm ml-2", warningColor)}>
+                <span className={cn("hidden md:block text-sm ml-2", warningColor)}>
                     {warningLabel}
                 </span>
             </div>
             {/* Danger */}
-            <div className="flex p-2 items-baseline" title={dangerLevels[DangerTypes.high].label}>
+            <div className="flex p-2 justify-center items-baseline md:justify-start" title={dangerLevels[DangerTypes.high].label}>
                 <span className={cn("text-2xl font-bold brightness-110 w-8 text-center", dangerColor)}>
                     {dangerCount}
                 </span>
-                <span className={cn("text-sm ml-2", dangerColor)}>
+                <span className={cn("hidden md:block text-sm ml-2", dangerColor)}>
                     {dangerLabel}
                 </span>
             </div>
