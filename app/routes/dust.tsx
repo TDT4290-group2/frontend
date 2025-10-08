1ef5a0662361f97cde2db33f3dc295e05c16f01b
haakoaal <haakoaal@stud.ntnu.no>
2025-10-08 17:27:14 +0200

    fix: merge conflicts, fixed design

--- app/routes/dust.tsx
+++ app/routes/dust.tsx
@@ -56,38 +56,15 @@ export default function Dust() {
 
 	return (
 		<section className="w-full flex flex-col">
-			<div className="flex flex-row">
-				<h1 className="text-3xl p-2">{"Dust exposure"}</h1>
-				<div className="ml-auto">
-				<Select
-					value={view}
-					onValueChange={(value) => setView(value as View | null)}
-				>
-					<SelectTrigger className="w-32">
-						<SelectValue placeholder="View" />
-					</SelectTrigger>
-					<SelectContent className="w-32">
-						<SelectItem key={"day"} value={"day"}>
-							{"Day"}
-						</SelectItem>
-						<SelectItem key={"week"} value={"week"}>
-							{"Week"}
-						</SelectItem>
-						<SelectItem key={"month"} value={"month"}>
-							{"Month"}
-						</SelectItem>
-					</SelectContent>
-				</Select>
-				</div>
-			</div>
 
 			<main className="flex w-full flex-col-reverse gap-4 md:flex-row">
-					<div className="flex flex-col gap-4 bg-[var(--card)] p-2 h-64 overflow-y-auto rounded-xl">
-						<Notifications />
-						<Summary exposureType="dust" safeCount={8} warningCount={4}/>
-					</div>
+				<div className="flex flex-col gap-4 p-2 rounded-xl">
+					<Summary exposureType="dust" safeCount={8} warningCount={4}/>
+					<Notifications />
+				</div>
 
 				<div className="flex flex-1 flex-col items-end gap-4">
+					<h1 className="text-3xl p-2 self-start">{"Dust exposure"}</h1>
 					<div className="flex gap-4">
 						{view === "day" && (
 							<Button
