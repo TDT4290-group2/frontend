016b521e4cb3418d3b1d86bf6a3d5e464f28d310
haakoaal <haakoaal@stud.ntnu.no>
2025-10-10 14:26:38 +0200

    refactor: adjusted overview to match new sensor layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -6,13 +6,17 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/ui/select";
+import { addDays, subDays } from "date-fns";
 import { useQueryState } from "nuqs";
 import { DailyNotes } from "../components/daily-notes";
 import { ChartLineDefault, ThresholdLine } from "../components/line-chart";
+import { Button } from "../components/ui/button";
 import { Calendar } from "../components/ui/calendar";
 import { Card } from "../components/ui/card";
 import Summary from "../components/ui/summary";
 import { WeekView } from "../components/weekly-view";
+import { useDayContext } from "../lib/day-context";
+import { Notifications } from "../components/ui/notifications";
 
 const tempDailyChartData = [
 	{ x: "08:00", y: 75 },
@@ -31,7 +35,7 @@ export function meta() {
 // biome-ignore lint: page components can be default exports
 export default function Home() {
 	const [view, setView] = useQueryState("view", parseAsView.withDefault("day"));
-
+	const { selectedDay, setSelectedDay } = useDayContext();
 	const greenNoiseDays = [new Date(2025, 8, 1), new Date(2025, 8, 5)];
 	const yellowNoiseDays = [new Date(2025, 8, 2), new Date(2025, 8, 6)];
 	const redNoiseDays = [
@@ -41,20 +45,23 @@ export default function Home() {
 	];
 
 	return (
-		<div className="flex w-full flex-col items-center gap-4 md:flex-row md:items-start">
-			<div className="flex flex-col gap-4 pl-2">
-				<Summary />
-			</div>
-			<div className="flex flex-1 flex-col gap-1">
-				<h1 className="text-center text-3xl md:pl-4 md:text-left">
-					{`Overview of the ${view}`}
-				</h1>
-				<div className="select-wrapper flex self-end">
+		<div className="flex w-full flex-col items-center md:items-start">
+			<div className="flex flex-col items-center gap-2 mb-4 w-full md:flex-row md:justify-between md:mb-0">
+				<h1 className="p-2 text-3xl">{`Overview of the ${view}`}</h1>
+				<div className="flex flex-row gap-4">
+					{view === "day" && (
+						<Button
+							onClick={() => setSelectedDay(subDays(selectedDay, 1))}
+							size={"icon"}
+						>
+							{"<"}
+						</Button>
+					)}
 					<Select
 						value={view}
 						onValueChange={(value) => setView(value as View | null)}
 					>
-						<SelectTrigger className="w-32 bg-background">
+						<SelectTrigger className="w-32">
 							<SelectValue placeholder="View" />
 						</SelectTrigger>
 						<SelectContent className="w-32">
@@ -69,50 +76,67 @@ export default function Home() {
 							</SelectItem>
 						</SelectContent>
 					</Select>
+					{view === "day" && (
+						<Button
+							onClick={() => setSelectedDay(addDays(selectedDay, 1))}
+							size={"icon"}
+						>
+							{">"}
+						</Button>
+					)}
 				</div>
-				<div className="view-wrapper w-full">
-					<section className="flex w-full flex-col place-items-center gap-4 pb-5">
-						{view === "month" ? (
-							<Card className="w-full">
-								<Calendar
-									fixedWeeks
-									showWeekNumber
-									disabled
-									mode="single"
-									weekStartsOn={1}
-									modifiers={{
-										safe: greenNoiseDays,
-										warning: yellowNoiseDays,
-										danger: redNoiseDays,
-									}}
-									modifiersClassNames={{
-										safe: cn("bg-[var(--safe)]"),
-										warning: cn("bg-[var(--warning)]"),
-										danger: cn("bg-[var(--danger)]"),
-										disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
-									}}
-									className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
-									captionLayout="dropdown"
-									buttonVariant="ghost"
-								/>
-							</Card>
-						) : view === "week" ? (
-							<>
-								{/* NOTE: Should use props, if none are provided then some default values are used. Like here */}
-								<WeekView />
-							</>
-						) : (
-							<ChartLineDefault
-								chartData={tempDailyChartData}
-								chartTitle="Vibration Exposure"
-								unit="db (TWA)"
-							>
-								<ThresholdLine y={120} dangerLevel="DANGER" />
-								{/* <ThresholdLine y={85} dangerLevel="WARNING" /> */}
-							</ChartLineDefault>
-						)}
-						<DailyNotes />
-					</section>
+			</div>
+			<div className="flex w-full flex-col gap-4 md:flex-row">
+				<div className="flex flex-col gap-4">
+					<Summary />
+					<Notifications />
+				</div>
+				<div className="flex flex-1 flex-col gap-1">
+					
+					<div className="view-wrapper w-full">
+						<section className="flex w-full flex-col place-items-center gap-4 pb-5">
+							{view === "month" ? (
+								<Card className="w-full">
+									<Calendar
+										fixedWeeks
+										showWeekNumber
+										disabled
+										mode="single"
+										weekStartsOn={1}
+										modifiers={{
+											safe: greenNoiseDays,
+											warning: yellowNoiseDays,
+											danger: redNoiseDays,
+										}}
+										modifiersClassNames={{
+											safe: cn("bg-[var(--safe)]"),
+											warning: cn("bg-[var(--warning)]"),
+											danger: cn("bg-[var(--danger)]"),
+											disabled: cn("m-2 rounded-2xl text-black dark:text-white"),
+										}}
+										className="w-full bg-transparent font-bold text-foreground [--cell-size:--spacing(6)] sm:[--cell-size:--spacing(10)] md:[--cell-size:--spacing(12)]"
+										captionLayout="dropdown"
+										buttonVariant="ghost"
+									/>
+								</Card>
+							) : view === "week" ? (
+								<>
+									{/* NOTE: Should use props, if none are provided then some default values are used. Like here */}
+									<WeekView />
+								</>
+							) : (
+								<ChartLineDefault
+									chartData={tempDailyChartData}
+									chartTitle="Vibration Exposure"
+									unit="db (TWA)"
+								>
+									<ThresholdLine y={120} dangerLevel="DANGER" />
+									{/* <ThresholdLine y={85} dangerLevel="WARNING" /> */}
+								</ChartLineDefault>
+							)}
+							<DailyNotes />
+						</section>
+					</div>
 				</div>
 			</div>
 		</div>
