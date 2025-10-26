4a012ecc6666bbb0269f0c976c1d5c8526fbbe77
phibkro <71797726+phibkro@users.noreply.github.com>
2025-10-26 02:10:18 +0100

    style: :art: fix lint and formatting

--- app/@types/i18n.d.ts
+++ app/@types/i18n.d.ts
@@ -3,7 +3,7 @@ import type { resources } from "@/i18n/config";
 declare module "i18next" {
 	// biome-ignore lint: this is how i18next types are extended
 	interface CustomTypeOptions {
-		resources: typeof resources["en"];
+		resources: (typeof resources)["en"];
 		enableSelector: "optimize";
 	}
 }
